a9352c264a9a4ebd2401f201d507d1d6332e9894
Mateusz Choma <kontakt@coderoad.pl>
2021-08-04 18:28:06 +0200

    Application initial state proposed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react'
 
-export const App = () => {
-  return (
-    <div>
-      CodeRoad APP
-    </div>
-  )
+export class App extends React.Component {
+  state = {
+    // global state
+    isLoading: false,
+    hasError: false,
+    errorMessage: '',
+    isInfoDisplayed: false,
+    infoMessage: '',
+
+    // user/auth state
+    isUserLoggedIn: false,
+    userDisplayName: '',
+    userEmail: '',
+    userAvatar: '',
+
+    // router state
+    notLoginUserRoute: 'LOGIN', // 'NEW-ACCOUNT' or 'FORGOT PASSWORD'
+
+    // login page state
+    loginEmail: '',
+    loginPassword: '',
+
+    // create account page
+    createAccountEmail: '',
+    createAccountPassword: '',
+    createAccountRepeatPassword: '',
+
+    // recover password page
+    recoverPasswordEmail: '',
+
+    // course list page
+    courses: null,
+    searchPhrase: ''
+  }
+
+  render () {
+    return (
+      <div>
+        CodeRoad APP
+      </div>
+    )
+  }
 }
 
 export default App
