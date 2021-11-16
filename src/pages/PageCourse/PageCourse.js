66d4e1cfe23cc64a11f9ae0642ba535367b76e4a
Mateusz Choma <kontakt@coderoad.pl>
2021-11-16 20:05:15 +0100

    `CourseLayout` separated

--- src/pages/PageCourse/PageCourse.js
+++ src/pages/PageCourse/PageCourse.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import CourseLayout from '../../templates/CourseLayout/CourseLayout'
+
 import { Outlet } from 'react-router-dom'
-
-import Ratio16x9 from '../../components/Ratio16x9'
-
-import { useTheme, Box } from '@mui/material'
 
 export const PageCourse = (props) => {
   const {
@@ -13,88 +11,19 @@ export const PageCourse = (props) => {
     ...otherProps
   } = props
 
-  const theme = useTheme()
-
   return (
-    <Box
-      sx={{
-        height: '100vh',
-        overflow: 'hidden',
-        [theme.breakpoints.down('sm')]: {
-          overflow: 'auto'
-        },
-        ...sx
-      }}
+    <CourseLayout
+      slotContent={
+        <Outlet />
+      }
+      slotSidebar={
+        (new Array(111)).fill(<p>SIDEBAR</p>)
+      }
+      slotTitle={
+        (new Array(111)).fill(<p>TITLE</p>)
+      }
       {...otherProps}
-    >
-      <Box
-        sx={{
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'row',
-          [theme.breakpoints.down('sm')]: {
-            height: 'auto',
-            minHeight: '100vh',
-            flexDirection: 'column'
-          }
-        }}
-      >
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            [theme.breakpoints.down('sm')]: {
-              flexGrow: 0
-            }
-          }}
-        >
-          <Ratio16x9>
-            <Box
-              sx={{
-                backgroundColor: 'black',
-                width: '100%',
-                height: '100%'
-              }}
-            >
-              <Outlet />
-            </Box>
-          </Ratio16x9>
-          <Box
-            sx={{
-              flexGrow: 1,
-              overflowX: 'hidden',
-              overflowY: 'auto'
-            }}
-          >
-            TITLE
-            {
-              (new Array(111)).fill(<p>TITLE</p>)
-            }
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            width: 320,
-            height: '100%',
-            backgroundColor: 'gray',
-            overflowX: 'hidden',
-            overflowY: 'auto',
-            [theme.breakpoints.down('sm')]: {
-              width: '100%',
-              height: 'auto',
-              flexGrow: 1
-            }
-          }}
-        >
-          SIDEBAR
-          {
-            (new Array(111)).fill(<p>SIDEBAR</p>)
-          }
-        </Box>
-      </Box>
-    </Box>
+    />
   )
 }
 
