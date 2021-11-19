2da58172f287c40cce47fe5dc6b176c6fb81f581
Mateusz Choma <kontakt@coderoad.pl>
2021-11-19 14:26:02 +0100

    Displaying all lessons

--- src/pages/PageAdminLessons/PageAdminLessons.js
+++ src/pages/PageAdminLessons/PageAdminLessons.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 
 import { useDispatch, useSelector } from 'react-redux'
 
-import { Box } from '@mui/material'
+import { Box, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, IconButton, Tooltip } from '@mui/material'
+import { Videocam as VideocamIcon, Edit as EditIcon, Delete as DeleteIcon } from '@mui/icons-material'
 
 import { getAllSelector, actionCreatorGetAll } from '../../state/lessons'
 
@@ -25,11 +26,55 @@ export const PageAdminLessons = (props) => {
   return (
     <Box
       sx={{
+        paddingTop: 2,
+        paddingBottom: 2,
         ...sx
       }}
       {...otherProps}
     >
-      {JSON.stringify(getAllLessonsState)}
+      <TableContainer component={Paper}>
+        <Table
+          sx={{ width: '100%' }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Type</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell align={'right'}>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {getAllLessonsState.value && getAllLessonsState.value.map((lesson) => (
+              <TableRow
+                key={lesson.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>
+                  {
+                    lesson.type === 'video' ?
+                      <VideocamIcon color={'primary'} />
+                      :
+                      lesson.type
+                  }
+                </TableCell>
+                <TableCell>{lesson.title}</TableCell>
+                <TableCell align={'right'}>
+                  <Tooltip title={'Edit'}>
+                    <IconButton color={'primary'}>
+                      <EditIcon />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title={'Delete'}>
+                    <IconButton color={'primary'}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </Tooltip>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   )
 }
